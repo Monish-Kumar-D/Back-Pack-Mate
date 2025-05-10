6f0605652406284b2a125a375076de554be5f8f3
Monish-Kumar-D <monishdharanilogan@gmail.com>
2025-05-11 01:35:15 +0530

    Add collected static files

---
+++ staticfiles/BackpackMate/script.c2c4cbb65346.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Use buttons to toggle between views
+    document.querySelector('#visited').addEventListener('click', () => load_mailbox('inbox'));
+
+  });
