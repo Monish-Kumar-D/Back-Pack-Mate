311682fe874886d8444e8e229f2fab69eb1d53db
Monish Kumar <141710569+Monish-Kumar-D@users.noreply.github.com>
2025-05-11 01:09:07 +0530

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a$0po-@*9h(oq6yf))o-$&*u4ov1#@e%uzdn-5$fwz(y^mt^uz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['back-pack-mate.onrender.com', 'www.back-pack-mate.onrender.com']
+ALLOWED_HOSTS = ['back-pack-mate.onrender.com', 'www.back-pack-mate.onrender.com', 'localhost', '127.0.0.1']
 
 
 
