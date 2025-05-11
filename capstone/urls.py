8aab0cad9489fca654ba0c0462e1706b42003104
Monish-Kumar-D <monishdharanilogan@gmail.com>
2025-05-11 16:55:06 +0530

    url settings update

--- capstone/urls.py
+++ capstone/urls.py
@@ -21,5 +21,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('BackpackMate.urls'))
-] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+# Serve media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Force media serving in production temporarily
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
