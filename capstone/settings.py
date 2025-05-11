e07bec32fa4b36f7f7d109c4d1a80bf811ffa9df
Monish-Kumar-D <monishdharanilogan@gmail.com>
2025-05-11 18:07:33 +0530

    nont

--- capstone/settings.py
+++ capstone/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-a$0po-@*9h(oq6yf))o-$&*u4ov1#@e%uzdn-5$fwz(y^mt^uz"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'back-pack-mate.onrender.com',
