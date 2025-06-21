035aced7547b07d470ab7a0176a1bad44f25e43f
Monish Kumar <141710569+Monish-Kumar-D@users.noreply.github.com>
2025-06-22 00:07:00 +0530

    Update setting.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -8,21 +8,6 @@ import cloudinary.api
 import os
 
 
-# # Cloudinary settings
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': 'dy3ozktu1',
-#     'API_KEY': '974733317868953',
-#     'API_SECRET': 'VYBOqul6zCuXBEIColxrh94vnvo',
-# }
-
-# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-# CLOUDINARY_STORAGE = {
-#     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME','dy3ozktu1'),
-#     'API_KEY': os.environ.get('CLOUDINARY_API_KEY','974733317868953'),
-#     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET','VYBOqul6zCuXBEIColxrh94vnvo'),
-# }
-
 
 # Build paths inside the project
 BASE_DIR = Path(__file__).resolve().parent.parent
